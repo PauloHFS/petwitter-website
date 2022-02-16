b51d609b9a590a92a36114e32d2faed0be8676c7
Paulo Hernane <paulohernane10@gmail.com>
2022-02-16 07:44:22 -0300

    setup chakra ui

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import { colors } from './theme';
+
+const theme = extendTheme({colors})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
