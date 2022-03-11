3ae2673787a497460b8bda438bf2aeb9ef125db0
Paulo Hernane <paulo@mobixtec.com>
2022-03-10 22:07:47 -0300

    Adicionando envio de posts no desktop

--- src/components/TweetZone.jsx
+++ src/components/TweetZone.jsx
@@ -1,36 +1,80 @@
-import { Avatar, Button, Flex, HStack, Textarea, Text } from '@chakra-ui/react';
+import {
+  Avatar,
+  Button,
+  Flex,
+  HStack,
+  Text,
+  Textarea,
+  useToast,
+} from '@chakra-ui/react';
+import { Formik } from 'formik';
+import { useMutation, useQueryClient } from 'react-query';
+import { getFromStorage } from '../services/auth';
+import { createPost } from '../services/posts';
 
 export const TweetZone = () => {
+  const toast = useToast();
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(createPost, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('feed');
+    },
+    onError: error => {
+      const { name, message } = error.toJSON();
+      toast({
+        title: name,
+        description: message,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      });
+    },
+  });
+
   return (
-    <Flex
-      display={['none', 'flex']}
-      flexDir="column"
-      borderBottom="solid"
-      borderBottomColor="gray.100"
-      borderBottomWidth="0.75rem"
-      py="2rem"
-      px="1.5rem"
+    <Formik
+      initialValues={{ text: '' }}
+      onSubmit={(values, { setSubmitting }) => {
+        mutation.mutate(values.text);
+        values.text = '';
+        setSubmitting(false);
+      }}
     >
-      <HStack>
-        <Avatar name={'Alo'} />
-        <Textarea
-          name="text"
-          /* value={values.text}
-        onChange={handleChange} */
-          placeholder="O que está acontecendo?"
-          borderColor="white"
-          resize="none"
-          _focus={{ borderColor: 'white' }}
-        ></Textarea>
-      </HStack>
-      <Flex flexDir="row-reverse">
-        <Button px="2" type="submit" /* disabled={isSubmitting} */>
-          Petwittar
-        </Button>
-        <Text mx="4" my="auto" color="gray.300">
-          0/140
-        </Text>
-      </Flex>
-    </Flex>
+      {({ values, handleChange, isSubmitting, handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <Flex
+            display={['none', 'flex']}
+            flexDir="column"
+            borderBottom="solid"
+            borderBottomColor="gray.100"
+            borderBottomWidth="0.75rem"
+            py="2rem"
+            px="1.5rem"
+          >
+            <HStack>
+              <Avatar name={getFromStorage('user').name} />
+              <Textarea
+                name="text"
+                value={values.text}
+                onChange={handleChange}
+                placeholder="O que está acontecendo?"
+                borderColor="white"
+                resize="none"
+                _focus={{ borderColor: 'white' }}
+              ></Textarea>
+            </HStack>
+            <Flex flexDir="row-reverse">
+              <Button px="2" type="submit" disabled={isSubmitting}>
+                Petwittar
+              </Button>
+              <Text mx="4" my="auto" color="gray.300">
+                {values.text.length}/140
+              </Text>
+            </Flex>
+          </Flex>
+        </form>
+      )}
+    </Formik>
   );
 };
